7de36b2272f088fbd1bc5a7ba0b44c0ea1f3db72
Grigorii Duca <contact@greg.md>
2015-12-18 18:59:06 +0200

    Update index.ts

--- src/app/components/home/index.ts
+++ src/app/components/home/index.ts
@@ -1,11 +1,10 @@
 import {Component} from 'angular2/angular2';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-import {EventCreateCmp} from 'app/components/events/create';
 import {config} from 'app/config';
 
 @Component({
     templateUrl: config.basePath + '/app/views/home/index.html',
-    directives: [EventCreateCmp, ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class HomeCmp {}
