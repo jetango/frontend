a8c13f4d2a38741fac7135b7677cbeac3916343e
Grigorii Duca <contact@greg.md>
2015-12-18 19:25:27 +0200

    .

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,4 +1,5 @@
-import {bootstrap, provide} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {Api} from 'app/resources/api';
