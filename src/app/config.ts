bda10392e7352697fee92b5c11359f57b6dfd392
Grigorii Duca <contact@greg.md>
2015-12-21 14:32:26 +0200

    .

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,15 @@
-export var config = {
-    basePath: '<%= basePath %>',
+let BASE_PATH = '<%= basePath %>';
 
-    useHashLocationStrategy: true,
+export var config = new function() {
+    this.basePath = BASE_PATH;
 
-    apiHost: 'http://127.0.0.1/api'
-};
+    this.appPath = this.basePath + '/app';
+
+    this.componentsPath = this.appPath + '/components';
+
+    this.viewsPath = this.appPath + '/views';
+
+    this.useHashLocationStrategy = true;
+
+    this.apiHost = 'http://127.0.0.1/api';
+}();
