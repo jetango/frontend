6dffd74aee0c7b95120e948ea05f7515a7895db1
Grigorii Duca <contact@greg.md>
2016-01-14 12:24:31 +0200

    .

--- src/app/config.ts
+++ src/app/config.ts
@@ -13,7 +13,7 @@ export var config = new function() {
 
     this.useHashLocationStrategy = ROUTE_HASH;
 
-    this.enableProductionMode = true;
+    this.enableProductionMode = false;
 
     this.apiHost = 'http://127.0.0.1/api';
 }();
